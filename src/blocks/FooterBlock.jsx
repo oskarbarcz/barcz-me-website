d71e916dd71bf49350a957fb63fcb3832e4ad2eb
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-18 00:27:30 +0100

    add hero and footer sections desktop view

---
+++ src/blocks/FooterBlock.jsx
@@ -0,0 +1,9 @@
+export default function FooterBlock() {
+  return (
+    <div className="bg-dark text-light">
+      <div className="container">
+        <p className="py-3">© 2017-2023 Oskar Barcz</p>
+      </div>
+    </div>
+  )
+}
