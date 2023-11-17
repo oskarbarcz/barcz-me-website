cde859de3212d44d15777694d71495f32412ece3
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-18 00:27:30 +0100

    add hero and footer sections desktop view

--- src/blocks/HeroBlock.jsx
+++ src/blocks/HeroBlock.jsx
@@ -1,8 +1,24 @@
+import photo from '../assets/photo_oskar.jpg'
+
 export default function HeroBlock() {
 
   return (
-    <>
-      <h1>Cześć, jestem <span>Oskar</span></h1>
-    </>
+      <section className="py-5 bg-noise">
+        <div className="container my-5 py-5 d-flex align-items-center">
+          <div>
+            <h1 className="display-1 fw-bold my-5">
+            Cześć, jestem <span className="text-primary text-glow">Oskar</span>
+          </h1>
+            <article className="fs-3">
+            <span className="bg-dark text-light py-1 px-2">
+              zawodowo tworzę oprogramowanie, występuję na konferencjach <br /> i działam w społeczności
+            </span>
+            </article>
+          </div>
+          <div>
+            <img className="photo m-5 ps-5" src={photo} alt="" width="300"/>
+          </div>
+        </div>
+      </section>
   )
 }
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,11 @@
 import HeroBlock from "../blocks/HeroBlock.jsx";
+import FooterBlock from "../blocks/FooterBlock.jsx";
 
 export default function Root() {
   return (
     <>
       <HeroBlock/>
+      <FooterBlock/>
     </>
   )
 }
