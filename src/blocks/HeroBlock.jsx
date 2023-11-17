ce004fa278f00e6f945963d5747846f44d10de02
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-17 21:51:38 +0100

    add initial hero section content

---
+++ src/blocks/HeroBlock.jsx
@@ -0,0 +1,8 @@
+export default function HeroBlock() {
+
+  return (
+    <>
+      <h1>Cześć, jestem <span>Oskar</span></h1>
+    </>
+  )
+}
---
+++ src/routes/Root.jsx
@@ -0,0 +1,9 @@
+import HeroBlock from "../blocks/HeroBlock.jsx";
+
+export default function Root() {
+  return (
+    <>
+      <HeroBlock/>
+    </>
+  )
+}
