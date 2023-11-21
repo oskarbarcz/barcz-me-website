5c198ad7ed5f5fccb498231ff274344e28b6c366
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-21 23:41:13 +0100

    add conferences component

--- src/blocks/ConferencesBlock.jsx
+++ src/blocks/ConferencesBlock.jsx
@@ -4,13 +4,38 @@ export default function ConferencesBlock() {
   return (
     <>
       <div className="container my-5 py-5">
-        <div className="row align-items-center">
+        <div className="row gx-5 align-items-center">
           <div className="col-12 col-md-6">
-            <img className="photo m-5 pe-5" src={photo} alt="" width="100"/>
+            <h2 className="text-light bg-dark p-1 h6 d-inline-block">Ostatnie wystąpienia</h2>
+            <div className="my-2 border border-2 p-2 w-75 rounded rounded-3">
+              <h3 className="fs-5 fw-bold text-secondary">Debata: Sposoby walki z cyberprzestępczością</h3>
+              <span className="text-muted">Warsaw Enterprise Institute, zdalnie</span>
+            </div>
+            <div className="my-2 mx-auto border border-2 p-2 w-75 rounded rounded-3">
+              <h3 className="fs-5 fw-bold text-secondary">Debata: Wolność w internecie</h3>
+              <span className="text-muted">Warsaw Enterprise Institute, Warszawa</span>
+            </div>
+            <div className="my-2 border border-2 p-2 w-75 rounded rounded-3">
+              <h3 className="fs-5 fw-bold text-secondary">Podróż od CRUDów do działania opartego o akcje</h3>
+              <span className="text-muted">NoExceptions by Boldare, zdalnie</span>
+            </div>
+            <div className="my-2 mx-auto border border-2 p-2 w-75 rounded rounded-3">
+              <h3 className="fs-5 fw-bold text-secondary">Catching the mutants: Wprowadzenie do testów mutacyjnych</h3>
+              <span className="text-muted">PHPers Summit 2022, Poznań</span>
+            </div>
           </div>
           <div className="col-12 col-md-6">
-            <h2 className="fw-bold display-3">Wystąpienia</h2>
-            <p>
+            <div className="position-relative">
+              <img src={photo} className="me-n5 mb-5 pe-5 w-100 rounded-circle" alt=""/>
+              <div className="position-absolute text-end" style={{right: '0', bottom: '15%'}}>
+                <span className="text-light p-1 bg-dark d-inline-block">
+                  Catching the mutants: introduction to mutation testing
+                </span>
+                <span className="text-light p-1 bg-dark d-inline-block fs-5">PHPers Summit 2022, Poznań</span>
+              </div>
+            </div>
+            <h2 className="fw-bold display-3 my-4">Wystąpienia</h2>
+            <p className="lead">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequatur cupiditate, earum et perferendis
               praesentium quam similique sint tempore? Adipisci alias blanditiis earum eveniet nam optio perferendis quae
               tenetur! Tempore?
--- src/components/ProfilePictureBlob.jsx
+++ src/components/ProfilePictureBlob.jsx
@@ -2,6 +2,6 @@ import photo from "../assets/photo_oskar.jpg";
 
 export default function ProfilePictureBlob() {
   return (
-    <img className="ms-n5 ps-5 profile-picture" src={photo} alt="Oskar Barcz - zdjęcie profilowe"/>
+    <img className="ms-n5 ps-5 w-100 rounded-circle" src={photo} alt="Oskar Barcz - zdjęcie profilowe"/>
   );
 }
