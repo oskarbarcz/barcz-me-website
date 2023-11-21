f48c4b5ca5453b7f311469bb9bbf4f23e961d3e4
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-21 21:11:31 +0100

    add networking section

---
+++ src/blocks/NavigationBlock.jsx
@@ -0,0 +1,46 @@
+export default function NavigationBlock() {
+  return (
+    <nav className="container navbar sticky-top navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-between">
+        <a className="navbar-brand fw-bold text-primary" href="#">Oskar Barcz</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flex-grow-0" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                O mnie
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Technologia
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Wystąpienia
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Projekty
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Leadership
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Kontakt
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/routes/RootRoute.jsx
+++ src/routes/RootRoute.jsx
@@ -1,12 +1,18 @@
 import HeroBlock from "../blocks/HeroBlock.jsx";
 import FooterBlock from "../blocks/FooterBlock.jsx";
 import TechnologyBlock from "../blocks/TechnologyBlock.jsx";
+import ConferencesBlock from "../blocks/ConferencesBlock.jsx";
+import NetworkingBlock from "../blocks/NetworkingBlock.jsx";
+import NavigationBlock from "../blocks/NavigationBlock.jsx";
 
 export default function RootRoute() {
   return (
     <>
+      <NavigationBlock/>
       <HeroBlock/>
       <TechnologyBlock/>
+      <ConferencesBlock/>
+      <NetworkingBlock/>
       <FooterBlock/>
     </>
   )
