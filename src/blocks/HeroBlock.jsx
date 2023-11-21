9f7b78b6e9f42e48fb37c524dd45fa029d041ee8
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-21 22:19:37 +0100

    add responsive hero styles

--- src/blocks/HeroBlock.jsx
+++ src/blocks/HeroBlock.jsx
@@ -1,22 +1,26 @@
-import photo from '../assets/photo_oskar.jpg'
+import ProfilePictureBlob from "../components/ProfilePictureBlob.jsx";
 
 export default function HeroBlock() {
 
   return (
-      <section className="py-5 bg-noise">
-        <div className="container my-5 py-5 d-flex align-items-center">
-          <div>
-            <h1 className="display-1 fw-bold my-5">
-            Cześć, jestem <span className="text-primary text-glow">Oskar</span>
-          </h1>
-            <article className="fs-3">
-            <span className="bg-dark text-light py-1 px-2">
-              zawodowo tworzę oprogramowanie, występuję na konferencjach <br /> i działam w społeczności
-            </span>
-            </article>
-          </div>
-          <div>
-            <img className="photo m-5 ps-5" src={photo} alt="" width="300"/>
+      <section className="py-3 py-lg-5">
+        <div className="container my-lg-5 py-lg-5 d-flex align-items-center">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-8 order-2 order-md-1">
+              <div>
+                <h1 className="display-1 fw-bold my-5">
+                  Cześć, jestem <span className="text-primary text-glow">Oskar</span>
+                </h1>
+                <article className="lead fs-3">
+                  <span className="bg-dark text-light py-1 px-1">
+                    zawodowo tworzę oprogramowanie, występuję na konferencjach i działam w społeczności
+                  </span>
+                </article>
+              </div>
+            </div>
+            <div className="col-12 col-md-4 order-1 order-lg-2 text-center">
+              <ProfilePictureBlob/>
+            </div>
           </div>
         </div>
       </section>
---
+++ src/components/ProfilePictureBlob.jsx
@@ -0,0 +1,7 @@
+import photo from "../assets/photo_oskar.jpg";
+
+export default function ProfilePictureBlob() {
+  return (
+    <img className="ms-n5 ps-5 profile-picture" src={photo} alt="Oskar Barcz - zdjęcie profilowe"/>
+  );
+}
