a39bd5b7fcb4f3aee48062583d2caa0abed57f89
Oskar Barcz <kontakt@archi-tektur.pl>
2023-11-28 01:33:09 +0100

    adjust spacing in conferences section

--- src/blocks/ConferencesBlock.jsx
+++ src/blocks/ConferencesBlock.jsx
@@ -7,7 +7,7 @@ export default function ConferencesBlock() {
       <div className="container my-lg-5 py-lg-5" id={Anchor().root.conferences}>
         <div className="row gx-5 align-items-center">
           <div className="col-12 col-lg-6 order-2 order-lg-1 pe-lg-3">
-            <section>
+            <section className="pe-5 me-5">
               <h2 className="text-light bg-dark mt-4 mt-lg-0 p-1 h6 d-inline-block">Ostatnie wystąpienia i publikacje</h2>
               <article className="my-2 border border-2 p-2 w-75 rounded rounded-3">
                 <h3 className="fs-5 fw-bold text-secondary">Debata: Sposoby walki z cyberprzestępczością</h3>
@@ -27,22 +27,22 @@ export default function ConferencesBlock() {
               </article>
             </section>
           </div>
-          <div className="col-12 col-lg-6 order-1 order-lg-2 ps-lg-5">
+          <div className="col-12 col-lg-6 order-1 order-lg-2">
             <section>
-              <article className="position-relative">
-                <img src={photo} className="me-n5 mb-5 pe-5 w-100 rounded-circle" alt=""/>
-                <div className="position-absolute text-end" style={{right: '0', bottom: '10%'}}>
-                <span className="text-light p-1 bg-dark d-inline-block">
-                  Catching the mutants: introduction to mutation testing
-                </span>
-                  <span className="text-light p-1 bg-dark d-inline-block fs-5">PHPers Summit 2022, Poznań</span>
-                </div>
-              </article>
               <h2 className="fw-bold display-3 my-4">Wystąpienia</h2>
               <article className="lead">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequatur cupiditate, earum et perferendis
                 praesentium quam similique sint tempore? Adipisci alias blanditiis earum eveniet nam optio perferendis quae
                 tenetur! Tempore?
+              </article>
+              <article className="position-relative">
+                <img src={photo} className="me-n5 mt-5 pt-5 pe-5 w-100 rounded-circle" alt=""/>
+                <div className="position-absolute text-end" style={{right: '0', bottom: '7.5%'}}>
+                <span className="text-light p-1 bg-dark d-inline-block">
+                  Catching the mutants: introduction to mutation testing
+                </span>
+                  <span className="text-light p-1 bg-dark d-inline-block fs-5">PHPers Summit 2022, Poznań</span>
+                </div>
               </article>
             </section>
             </div>
