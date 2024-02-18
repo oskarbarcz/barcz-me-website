e7f066b282274a182ea380480fcb7eb293231664
Oskar Barcz <kontakt@archi-tektur.pl>
2024-02-18 18:41:26 +0100

    feature(banner): add oncoming event banner

--- src/routes/RootRoute.jsx
+++ src/routes/RootRoute.jsx
@@ -3,8 +3,14 @@ import FooterBlock from "../blocks/FooterBlock.jsx";
 import TechnologyBlock from "../blocks/TechnologyBlock.jsx";
 import ConferencesBlock from "../blocks/ConferencesBlock.jsx";
 import NavigationBlock from "../blocks/NavigationBlock.jsx";
+import Offcanvas from "../components/Offcanvas.jsx";
 
 export default function RootRoute() {
+
+  const targetDateTime = new Date(2024, 1, 21, 20, 0);
+  const now = new Date();
+  const showEventBanner = now < targetDateTime;
+
   return (
     <>
       <NavigationBlock/>
@@ -12,6 +18,7 @@ export default function RootRoute() {
       <TechnologyBlock/>
       <ConferencesBlock/>
       <FooterBlock/>
+      {showEventBanner && <Offcanvas/>}
     </>
   )
 }
